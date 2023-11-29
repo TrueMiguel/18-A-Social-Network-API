415a89bfad3e9694f174476d8b26da8e6e481825
Miguel Maldonado <miguel.maldonado5802@gmail.com>
2023-11-29 16:38:56 -0500

    added the reactionSchema to the Thought.js model.

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,25 @@
 const { Schema, model } = require('mongoose');
 const { DateTime } = require('luxon');
+const { ObjectId } = require('bson');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+        },
+        reactionBody: {type: String, required: true, maxlength: 280},
+        username: {type: String, required: true},
+        createdAt: {
+            type: Date, 
+            default: Date.now,
+            // formating the timestamp along with using luxon for the time format.
+            get: function (timestamp) {
+                return DateTime.fromJSDate(timestamp).toFormat('MM/dd/yyy hh:mm a');
+            },
+        }
+    }
+)
 
 const thoughtSchema = new Schema(
     {
@@ -13,12 +33,7 @@ const thoughtSchema = new Schema(
             },
         },
         username: {type:String, required: true}, // need to be the user that created this thought
-        reactions: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'reaction',
-            },
-        ],
+        reactions: [reactionSchema],
     },
     {
         toJSON: {
